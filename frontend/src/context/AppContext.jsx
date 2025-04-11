11c6db5448dcca8350a6767de799d97ed0c36726
Deepak <deepak.ch@algonox.com>
2025-04-11 22:04:09 +0530

    Added Some Frontend Functionalities

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,16 +1,52 @@
-import { createContext } from "react";
-import {doctors} from '../assets/assets';
+import { createContext, useEffect, useState } from "react";
+import axios from 'axios';
+import {toast} from 'react-toastify'
+import doctorModel from "../../../backend/models/doctorModel";
+
 export const AppContext=createContext()
 
 const AppContextProvider=(props)=>{
 
     const currencySymbol='$'
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const [doctors, setDoctors] = useState([])
 
-    const value={
+    const value = {
         doctors,
         currencySymbol
     }
 
+    const getDoctorsData = async () => {
+        try {
+
+            const {data} = await axios.get(backendUrl + '/api/doctor/list')
+            if (data.success) {
+                setDoctors(data.doctors)
+            }
+            else {
+                toast.error(data.message)
+            }
+        } catch(error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    };
+
+    useEffect(()=>{
+        getDoctorsData()
+    },[])
+
+    const doctorList = async () => {
+        try {
+            const doctors = await doctorModel.find({}).select(['-password','-email'])
+
+            res.json({success:true, doctors})
+        }
+        catch (error) {
+            console.log(error)
+            res.json({success:false, message:error.message})
+        }
+    }
     return(
         <AppContext.Provider value={value}>
 
