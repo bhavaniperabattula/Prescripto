731ebdf4923faf31489a6c573488f7f595bf33ce
PERABATTULA BHAVANI <bhavani9perabattula@gmail.com>
2025-04-11 09:05:25 +0530

    first commit

---
+++ Prescripto-mainpro/Prescripto-main (1)/Prescripto-main (1)/Prescripto-main/backend/server.js
@@ -0,0 +1,35 @@
+import express from "express"
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from "./config/mongodb.js"
+import connectCloudinary from "./config/cloudinary.js"
+import userRouter from "./routes/userRoute.js"
+import doctorRouter from "./routes/doctorRoute.js"
+import adminRouter from "./routes/adminRoute.js"
+import dotenv from "dotenv"
+
+
+dotenv.config();
+console.log("RAZORPAY_KEY_ID:", process.env.RAZORPAY_KEY_ID);
+
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// api endpoints
+app.use("/api/user", userRouter)
+app.use("/api/admin", adminRouter)
+app.use("/api/doctor", doctorRouter)
+
+app.get("/", (req, res) => {
+  res.send("API Working")
+});
+
+app.listen(port, () => console.log(`Server started on PORT:${port}`))
