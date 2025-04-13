2d8b939c36d6d36216a5ec506ffd99103d0ec197
Deepak <deepak.ch@algonox.com>
2025-04-13 14:55:23 +0530

    Modified the API URL - `update-profile` to `get-profile`

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -32,7 +32,7 @@ const AppContextProvider=(props)=>{
     const loadUserProfileData = async () => {
         try {
 
-            const { data } = await axios.get(backendUrl + '/api/user/update-profile', {headers:{token}})
+            const { data } = await axios.get(backendUrl + '/api/user/get-profile', {headers:{token}})
             if (data.success) {
                 setUserData(data.userData)
             } else {
